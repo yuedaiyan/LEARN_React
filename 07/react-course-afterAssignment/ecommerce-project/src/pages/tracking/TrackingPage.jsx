030b9a98bc4faf869d64848ea36235a49959f441
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-08 14:01:26 +0800

    lesson 7 assignment 7h finished

--- 07/react-course-afterAssignment/ecommerce-project/src/pages/tracking/TrackingPage.jsx
+++ 07/react-course-afterAssignment/ecommerce-project/src/pages/tracking/TrackingPage.jsx
@@ -1,7 +1,8 @@
+import Header from "../../components/Header";
+import { Link } from "react-router";
 import "./TrackingPage.css";
-import Header from "../components/Header";
 
-function TrackingPage({cart}) {
+function TrackingPage({ cart }) {
     return (
         <>
             <title>Tracking</title>
