391dfa2fc0718495b24bfa6d91664f1de04b3d6a
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-11 17:21:24 +0800

    lesson 9: Use beforeEach to remove duplication

--- react-course/ecommerce-project/src/pages/home/Product.test.jsx
+++ react-course/ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,4 +1,4 @@
-import { it, expect, describe, vi } from "vitest";
+import { it, expect, describe, vi, beforeEach } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Product from "./Product";
@@ -7,9 +7,12 @@ import axios from "axios";
 vi.mock("axios");
 
 describe("Product component", () => {
-    it("deisplays the product details correctly", () => {
+    let product;
+    let loadCart;
+
+    beforeEach(() => {
         // 劫持传入商品信息
-        const product = {
+        product = {
             id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
             image: "images/products/athletic-cotton-socks-6-pairs.jpg",
             name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -22,8 +25,10 @@ describe("Product component", () => {
         };
 
         // 劫持后端
-        const loadCart = vi.fn();
+        loadCart = vi.fn();
+    });
 
+    it("deisplays the product details correctly", () => {
         // 虚假渲染
         render(
             <Product
@@ -46,22 +51,6 @@ describe("Product component", () => {
     });
 
     it("adds a product to the cart", async () => {
-        // 劫持传入商品信息
-        const product = {
-            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-            rating: {
-                stars: 4.5,
-                count: 87,
-            },
-            priceCents: 1090,
-            keywords: ["socks", "sports", "apparel"],
-        };
-
-        // 劫持后端
-        const loadCart = vi.fn();
-
         // 虚假渲染
         render(
             <Product
@@ -69,7 +58,6 @@ describe("Product component", () => {
                 loadCart={loadCart}
             />,
         );
-
         // 初始化:模拟点击库
         const user = userEvent.setup();
         // 获得 添加到购物车 按钮
@@ -83,18 +71,6 @@ describe("Product component", () => {
             quantity: 1,
         });
         // 检测:是否调用了 loadCart()
-        expect(loadCart).toHaveBeenCalled;
+        expect(loadCart).toHaveBeenCalled();
     });
 });
-
-// const product = {
-//     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-//     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-//     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-//     rating: {
-//         stars: 4.5,
-//         count: 87,
-//     },
-//     priceCents: 1090,
-//     keywords: ["socks", "sports", "apparel"],
-// };
