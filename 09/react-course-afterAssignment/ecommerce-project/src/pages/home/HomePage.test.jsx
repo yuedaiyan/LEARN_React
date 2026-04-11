e1797a15b91a014d9b8ccb6efea6d72f62da1903
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-11 18:13:45 +0800

    lesson 9 assignment setup

---
+++ 09/react-course-afterAssignment/ecommerce-project/src/pages/home/HomePage.test.jsx
@@ -0,0 +1,71 @@
+import { it, expect, describe, vi, beforeEach } from "vitest";
+import { render, screen, within } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+// 导入专用的 Router h试库
+import { MemoryRouter } from "react-router";
+// 劫持整个axios库
+import axios from "axios";
+vi.mock("axios");
+import HomePage from "./HomePage";
+
+describe("HomePage component", () => {
+    let loadCart;
+    beforeEach(() => {
+        loadCart = vi.fn();
+
+        // 劫持:返回商品请求 → 只返回两个商品
+        axios.get.mockImplementation(async urlPath => {
+            if (urlPath === "/api/products") {
+                return {
+                    data: [
+                        {
+                            id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                            image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+                            name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+                            rating: {
+                                stars: 4.5,
+                                count: 87,
+                            },
+                            priceCents: 1090,
+                            keywords: ["socks", "sports", "apparel"],
+                        },
+                        {
+                            id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                            image: "images/products/intermediate-composite-basketball.jpg",
+                            name: "Intermediate Size Basketball",
+                            rating: {
+                                stars: 4,
+                                count: 127,
+                            },
+                            priceCents: 2095,
+                            keywords: ["sports", "basketballs"],
+                        },
+                    ],
+                };
+            }
+        });
+    });
+
+    it("displays the products correct", async () => {
+        render(
+            // 因为Homepage里面有Header,Header里面有Link,所以必须使用Router包裹
+            <MemoryRouter>
+                <HomePage
+                    cart={[]}
+                    loadCart={loadCart}
+                />
+                ,
+            </MemoryRouter>,
+        );
+
+        // find可以等到异步结束,一直等,直到直到位置
+        const productContainers = await screen.findAllByTestId("product-container");
+
+        // 测试:应该可以从主页的商品列表中找到两个商品
+        expect(productContainers.length).toBe(2);
+
+        // 测试:商品的名称
+        expect(within(productContainers[0]).getByText("Black and Gray Athletic Cotton Socks - 6 Pairs")).toBeInTheDocument();
+        expect(within(productContainers[1]).getByText("Intermediate Size Basketball")).toBeInTheDocument();
+    });
+});
---
+++ 09/react-course-beforeAssignment/ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
+import Header from "../../components/Header";
+import ProductsGrid from "./ProductsGrid";
+import "./HomePage.css";
+
+function HomePage({ cart, loadCart }) {
+    const [searchParams] = useSearchParams();
+    const search = searchParams.get("search") || "";
+
+    console.log("search in homePage:\n", search);
+
+    const [products, setPorducts] = useState([]);
+    useEffect(() => {
+        (async () => {
+            const url = search ? `/api/products?search=${search}` : "/api/products";
+            console.log(url);
+            const response = await axios.get(url);
+            setPorducts(response.data);
+        })();
+    }, [search]);
+
+    return (
+        <>
+            <title>Ecommerce Project</title>
+            <link
+                rel="icon"
+                type="image/svg+xml"
+                href="home-favicon.png"
+            />
+
+            <Header cart={cart} />
+
+            <div className="home-page">
+                <ProductsGrid
+                    products={products}
+                    loadCart={loadCart}
+                />
+            </div>
+        </>
+    );
+}
+
+export default HomePage;
+// TODO: 搜索框添加 Enter 交互
