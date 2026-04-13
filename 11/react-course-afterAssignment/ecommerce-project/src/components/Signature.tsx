f7b331c4bbd1a85cc797745c012de43794014031
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-13 17:01:47 +0800

    所有页面添加签名

---
+++ 11/react-course-afterAssignment/ecommerce-project/src/components/Signature.tsx
@@ -0,0 +1,21 @@
+import "./Signature.css"
+function Signature() {
+    return (
+        <>
+            <div className="signature">
+                <div className="name">- Yue Dayan -</div>
+                <div className="time">2026 - 04 - 13</div>
+            </div>
+            <script
+                type="module"
+                src="scripts/amazon.js"
+            ></script>
+            <script
+                type="module"
+                src="scripts/search.js"
+            ></script>
+        </>
+    );
+}
+
+export default Signature
