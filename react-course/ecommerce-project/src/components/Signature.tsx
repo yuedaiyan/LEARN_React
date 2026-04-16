78a4693dbf15df4a337f0baff678027c5c49f33f
Yuedaiyan <yuedaiyan2003@gmail.com>
2026-04-17 00:56:42 +0800

    React Course Finished

--- react-course/ecommerce-project/src/components/Signature.tsx
+++ react-course/ecommerce-project/src/components/Signature.tsx
@@ -4,7 +4,7 @@ function Signature() {
         <>
             <div className="signature">
                 <div className="name">- Yue Daiyan -</div>
-                <div className="time">2026 - 04 - 13</div>
+                <div className="time">2026 - 04 - 13 v1.1</div>
             </div>
         </>
     );
